918366c83e0ba04dcaa11f185f354f65a8a9d0bc
Zara Turtle <zara.turtle@protonmail.com>
2020-04-19 22:34:05 +0100

    start on Wiring chapter

--- example/src/main/scala/fpmortals/time/LocalClock.scala
+++ example/src/main/scala/fpmortals/time/LocalClock.scala
@@ -7,8 +7,12 @@ package time
 import cats._, implicits._
 import cats.effect.IO
 
-trait LocalClock[F[_]] {
+trait LocalClock[F[_]] { self =>
   def now: F[Epoch]
+
+  def mapK[G[_]](f: F ~> G): LocalClock[G] = new LocalClock[G] {
+    def now: G[Epoch] = f(self.now)
+  }
 }
 
 final class LocalClockTask extends LocalClock[IO] {
--- example/src/main/scala/fpmortals/time/Sleep.scala
+++ example/src/main/scala/fpmortals/time/Sleep.scala
@@ -10,8 +10,13 @@ import scala.concurrent.ExecutionContext
 import cats._, implicits._
 import cats.effect.IO
 
-trait Sleep[F[_]] {
+trait Sleep[F[_]] { self =>
   def sleep(time: FiniteDuration): F[Unit]
+
+  def mapK[G[_]](f: F ~> G): Sleep[G] = new Sleep[G] {
+    def sleep(time: FiniteDuration): G[Unit] = f(self.sleep(time))
+  }
+
 }
 
 final class SleepIO extends Sleep[IO] {
